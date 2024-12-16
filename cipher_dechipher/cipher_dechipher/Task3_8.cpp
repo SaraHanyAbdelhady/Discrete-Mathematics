1da5567b1fdffea6e652d21b1c6bd82e6e8e0b73
SaraHanyAbdelhady <saraabdelhady80@gmail.com>
2024-12-16 18:41:22 +0200

    adding comments

--- cipher_dechipher/cipher_dechipher/Task3_8.cpp
+++ cipher_dechipher/cipher_dechipher/Task3_8.cpp
@@ -6,24 +6,26 @@ using namespace std;
 char Italian_Alphabet[21][2];
 
 string Cipher(string msg) {
-    char A = 'A';
+    char A = 'A';   
     char a = 'a';
     // initializing Italian Alphabet
     for (int x = 0; x < 21; ++x) {
-        if (A == 'J' || A == 'K' || A == 'W' || A == 'X' || A == 'Y') {
+        if (A == 'J' || A == 'K' || A == 'W' || A == 'X' || A == 'Y') // excluded for Italian Alphabets
+        {
             A++;
             a++;
             x--;
             continue;
         }
-        Italian_Alphabet[x][0] = a++;
-        Italian_Alphabet[x][1] = A++;
+        Italian_Alphabet[x][0] = a++;   // small letters
+        Italian_Alphabet[x][1] = A++;   // capital letters
     }
     string ciphered = "";
     int index;
     int capital;
     for (int i = 0; i < msg.size(); i++) {
-        if (msg[i] == ' ') ciphered += msg[i];
+        index = 0;
+        if (msg[i] == ' ') ciphered += msg[i]; // preserve spaces
         else {
             for (index = 0; index < 21; index++) {
                 if (msg[i] == Italian_Alphabet[index][0]) {
@@ -35,11 +37,13 @@ string Cipher(string msg) {
                     break;
                 }
             }
-            if (index == 21) return "the Text contains non italian letters, please enter another one containing italian letters only.";
+            if (index == 21) //if not a italian letter
+                return "the Text contains non italian letters, please enter another one containing italian letters only.";
             else {
+                // applying cipher equation
                 index = index * 5 + 2;
                 index = index % 21;
-                ciphered += Italian_Alphabet[index][capital];
+                ciphered += Italian_Alphabet[index][capital]; // adding ciphered letters
             }
         }
     }
